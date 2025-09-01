a1e6be087c0c46dddf0e4af217c222e15fcea3be
ClaudeZirconMateo-UCMN <82535408+ClaudeZirconMateo-UCM@users.noreply.github.com>
2025-09-01 20:33:29 +0800

    Add files via upload

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,19 @@
+public class FixDebugOne
+{
+/* This program displays a greeting */
+    public static void main(String[]args){
+        System.out.println("Hello World!");
+    }
+}
+/* 
+The class name was written as "FixDebug One" with a space. It should be "FixDebugOne".
+
+The method name was written as "Main" with a capital M. In Java, it should be "main" in lowercase.
+
+The parameter in the main method was incorrect. It was written as (String args) but should be (String[] args).
+
+On the fifth line, "System" was misspelled as "Systm", and the semicolon at the end of the statement was missing.
+
+Finally, the program did not include a closing bracket } at the end.
+
+*/
