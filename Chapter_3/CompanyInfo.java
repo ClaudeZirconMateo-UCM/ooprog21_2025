a1d86454699dd61e3c80b749d4f8c78ff8ba3893
ClaudeZirconMateo-UCMN <82535408+ClaudeZirconMateo-UCM@users.noreply.github.com>
2025-09-19 08:52:28 +0800

    Add files via upload

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,20 @@
+class CompanyInfo{
+
+public static void main(String[] args){
+
+System.out.println("Smart Electronics");
+   displayHours();
+  
+  }
+
+public static void displayHours(){
+   String hoursDisplay = "Monday - Friday 8 am to 6 pm"+
+                         "\nSaturday      8 am to noon"+
+                         "\nSunday        closed";
+   System.out.print(hoursDisplay);
+   }
+}
+                                                            
+
+   
+   
