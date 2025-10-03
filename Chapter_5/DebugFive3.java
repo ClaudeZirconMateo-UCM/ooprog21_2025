3c51aacc83d31ee06d92ff5e9c33331fb5d6d47a
ClaudeZirconMateo-UCMN <82535408+ClaudeZirconMateo-UCM@users.noreply.github.com>
2025-10-03 18:19:02 +0800

    Add files via upload

---
+++ Chapter_5/DebugFive3.java
@@ -0,0 +1,29 @@
+// DebugFive4.java
+// Outputs highest of four numbers
+import java.util.*;
+class DebugFive4 //I removed public
+{
+   public static void main (String[] args) 
+   {
+      int one, two, three, four;
+      int highest;
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter an integer >> ");
+      one = input.nextInt();
+      System.out.print("Enter an integer >> ");
+      two = input.nextInt(); //'one' changed to 'two'
+      System.out.print("Enter an integer >> ");
+      three = input.nextInt(); //'one' changed to 'three'
+      System.out.print("Enter an integer >> ");
+      four = input.nextInt();//'one' changed to 'four'
+      
+      highest = one;
+      if (highest < two) //fixed if statement
+         highest = two;
+      if (highest < three) //fixed if statement
+         highest = three;
+      if (highest < four) //fixed if statement
+         highest = four;
+      System.out.println("The highest number is " + highest);
+   }
+}
